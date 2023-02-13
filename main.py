77a3a64b1856590549e4fd167b3ddb93e32943e1
Tima0102040 <voznesenskiytim@gmail.com>
2023-02-13 11:55:08 +0200

    Add new color

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+import pygame
+import sys
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+pygame.init()
+size_block=100
+margin=15
+width=heigth=size_block*3+margin*4
+
+size_window=(width, heigth)
+screen=pygame.display.set_mode(size_window)
+pygame.display.set_caption("Tic tac toe")
+
+black=(0,0,0)
+red=(255,0,0)
+green=(0,255,0)
+white=(255,255,255)
+arr=[[0]*3 for i in range (3)]
+query=0
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+        elif event.type==pygame.MOUSEBUTTONDOWN:
+            x_mouse, y_mouse=pygame.mouse.get_pos()
+            col=x_mouse//(size_block+margin)
+            row=y_mouse//(size_block+margin)
+            if arr[row][col]==0:
+                if query % 2 == 0:
+                    arr[row][col] = 'x'
+                else:
+                    arr[row][col] = 'o'
+                query += 1
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    for row in range(3):
+        for col in range(3):
+            if arr[row][col]=='x':
+                color=red
+            elif arr[row][col]=='o':
+                color=green
+            else:
+                color=white
+            x=col*size_block+(col+1)*margin
+            y=row*size_block+(row+1)*margin
+            pygame.draw.rect(screen, color, (x,y,size_block, size_block))
 
+    pygame.display.update()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
